d1f62d132235c79e9aaf5b6409f866ae3e493613
sajicode <sajioloye@gmail.com>
2019-03-07 23:41:44 +0100

    create first graphql api

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,15 +1,38 @@
-// using default and named exports
-import myLocation, {
-  message,
-  name,
-  getGreeting
-} from "./myModule";
-import add, {
-  subtract
-} from './math';
+import {
+  GraphQLServer
+} from "graphql-yoga";
 
-console.log(myLocation)
-console.log(getGreeting("Jessica"))
-console.log(message, name);
-console.log(add(9, 7));
-console.log(subtract(6, 9));
+// Type definitions (Schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is the first query. Perfect!";
+    },
+    name() {
+      return 'Maximus Meridius';
+    },
+    location() {
+      return 'Lagos, Nigeria'
+    },
+    bio() {
+      return 'Curious late bloomer'
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => console.log("The server is up and running"));
--- graphql-basics/src/math.js
+++
@@ -1,8 +0,0 @@
-const add = (x, y) => x + y;
-const subtract = (a, b) => a - b;
-
-export {
-  add as
-  default,
-  subtract
-}
