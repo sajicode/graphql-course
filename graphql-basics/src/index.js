fd3ada0de9fd8f3933bd7e0c6c05e7faf5ef7f37
sajicode <sajioloye@gmail.com>
2019-03-09 09:53:53 +0100

    work with arrays of scalar and custom types

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,10 +1,54 @@
 import { GraphQLServer } from "graphql-yoga";
 
+// Remove later
+const users = [
+  {
+    id: "1",
+    name: "Rex",
+    email: "rex@hex.com",
+    age: 56
+  },
+  {
+    id: "2",
+    name: "Tex",
+    email: "tex@hex.com",
+    age: 36
+  },
+  {
+    id: "3",
+    name: "Jex",
+    email: "Jex@hex.com",
+    age: 16
+  }
+];
+
+const posts = [
+  {
+    id: "01",
+    title: "Lofty Heights",
+    body: "O the escapades of man, for they are but mini-gods",
+    published: "02-25-1992"
+  },
+  {
+    id: "02",
+    title: "Lowly Plains",
+    body:
+      "But bring down yourselves o lowly man, for you are but servants in the garden of the most high",
+    published: "02-28-1992"
+  },
+  {
+    id: "03",
+    title: "Tender Hills",
+    body: "Little actions, thoughtful thoughts",
+    published: "02-29-1992"
+  }
+];
+
 // Type definitions (Schema)
 const typeDefs = `
   type Query {
-    add(x: Int!, y: Int!): Int!
-    greeting(name: String): String!
+    posts(query: String): [Post!]!
+    users(query: String): [User!]!
     me: User!
     post: Post!
   }
@@ -27,12 +71,19 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
-    add(parent, { x, y }, ctx, info) {
-      return x + y;
+    posts(parent, args, ctx, info) {
+      if (!args.query) return posts;
+      return posts.filter(
+        post =>
+          post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+          post.body.toLowerCase().includes(args.query.toLowerCase())
+      );
     },
-    greeting(parent, args, ctx, info) {
-      if (args.name) return `Hello ${args.name}`;
-      else return "Hello";
+    users(parent, args, ctx, info) {
+      if (!args.query) return users;
+      return users.filter(user =>
+        user.name.toLowerCase().includes(args.query.toLowerCase())
+      );
     },
     me() {
       return {
