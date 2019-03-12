64ae52f6681d449322e5561da88476437117d367
sajicode <sajioloye@gmail.com>
2019-03-12 23:41:27 +0100

    refactor mutations to use input types

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -74,6 +74,7 @@ const comments = [{
 ];
 
 // Type definitions (Schema)
+// Everything listed in an input type has to be a scalar type
 const typeDefs = `
   type Query {
     posts(query: String): [Post!]!
@@ -84,9 +85,28 @@ const typeDefs = `
   }
 
   type Mutation {
-    createUser(name: String!, email: String!, age: Int): User!
-    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
-    createComment(text: String!, author: ID!, post: ID!): Comment!
+    createUser(data: CreateUserInput): User!
+    createPost(data: CreatePostInput): Post!
+    createComment(data: CreateCommentInput): Comment!
+  }
+
+  input CreateUserInput {
+    name: String!
+    email: String!
+    age: Int
+  }
+
+  input CreatePostInput {
+    title: String!
+    body: String!
+    published: Boolean!
+    author: ID!
+  }
+
+  input CreateCommentInput {
+    text: String!
+    author: ID!
+    post: ID!
   }
 
   type User {
@@ -154,25 +174,25 @@ const resolvers = {
   },
   Mutation: {
     createUser(parent, args, ctx, info) {
-      const emailTaken = users.some(user => user.email === args.email);
+      const emailTaken = users.some(user => user.email === args.data.email);
       if (emailTaken) throw new Error("Email taken")
 
       const user = {
         id: uuidv4(),
-        ...args
+        ...args.data
       };
 
       users.push(user);
       return user;
     },
     createPost(parent, args, ctx, info) {
-      const userExists = users.some(user => user.id === args.author)
+      const userExists = users.some(user => user.id === args.data.author)
 
       if (!userExists) throw new Error("User not found");
 
       const post = {
         id: uuidv4(),
-        ...args
+        ...args.data
       };
 
       posts.push(post);
@@ -180,15 +200,15 @@ const resolvers = {
       return post;
     },
     createComment(parent, args, ctx, info) {
-      const userExists = users.some(user => user.id === args.author);
-      const postExists = posts.some(post => post.id === args.post && post.published)
+      const userExists = users.some(user => user.id === args.data.author);
+      const postExists = posts.some(post => post.id === args.data.post && post.published)
 
       if (!userExists) throw new Error("User not found");
       if (!postExists) throw new Error("Post not found");
 
       const comment = {
         id: uuidv4(),
-        ...args
+        ...args.data
       };
 
       comments.push(comment);
