e509277dc5edcfaeb4eaeb5374c5866de002c0ab
sajicode <sajioloye@gmail.com>
2019-03-21 23:38:44 +0100

    try out mutations with prisma bindings

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -1,12 +1,47 @@
-import {
-  Prisma
-} from 'prisma-binding';
+import { Prisma } from "prisma-binding";
 
 const prisma = new Prisma({
-  typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'http://localhost:4466'
+  typeDefs: "src/generated/prisma.graphql",
+  endpoint: "http://localhost:4466"
 });
 
-prisma.query.users(null, '{ id name posts { id title } }').then((data) => console.log(JSON.stringify(data, undefined, 2)))
+// prisma.query.comments(null, '{ id text author {id name}}').then(data => console.log(JSON.stringify(data, null, 2)));
 
-prisma.query.comments(null, '{ id text author {id name}}').then(data => console.log(JSON.stringify(data, null, 2)));
+// prisma.mutation
+//   .createPost(
+//     {
+//       data: {
+//         title: "Latest GraphQL post",
+//         body: "Hey Hey Hey. Graphs",
+//         published: true,
+//         author: {
+//           connect: {
+//             id: "cjtd81lq700460723bpjakjxb"
+//           }
+//         }
+//       }
+//     },
+//     "{id title body published}"
+//   )
+//   .then(data => {
+//     console.log(data);
+//     return prisma.query.users(null, "{ id name posts { id title } }");
+//   })
+//   .then(data => console.log(JSON.stringify(data, null, 2)));
+
+prisma.mutation
+  .updatePost(
+    {
+      where: {
+        id: "cjtd8p53f008c0723ynodwue5"
+      },
+      data: {
+        body: "The million faces of all"
+      }
+    },
+    "{ id }"
+  )
+  .then(data => {
+    return prisma.query.posts(null, "{ id title body published}");
+  })
+  .then(data => console.log(data));
