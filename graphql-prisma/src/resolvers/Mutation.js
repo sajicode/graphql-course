832120e2100673fe07b8131a041b228a067fa5b0
sajicode <sajioloye@gmail.com>
2019-04-05 10:44:44 +0100

    setup mutation for logging in users

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,17 +1,34 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 // Take in password => validate password => Hash password => Generate auth token(JWT)
 
 const Mutation = {
-  async createUser(parent, args, {
-    prisma
-  }, info) {
+  async login(parent, args, { prisma }, info) {
+    const user = await prisma.query.user({
+      where: {
+        email: args.data.email
+      }
+    });
+
+    if (!user) throw new Error("Invalid Email Address");
+
+    const isMatch = await bcrypt.compare(args.data.password, user.password);
+
+    if (!isMatch) throw new Error("Invalid Password");
+
+    return {
+      user,
+      token: jwt.sign({ userId: user.id }, "whatsecretis?")
+    };
+  },
+
+  async createUser(parent, args, { prisma }, info) {
     if (args.data.password.length < 8) {
-      throw new Error("Password must be 8 characters or longer")
+      throw new Error("Password must be 8 characters or longer");
     }
 
-    const password = await bcrypt.hash(args.data.password, 10)
+    const password = await bcrypt.hash(args.data.password, 10);
 
     // info ensures that whatever selection set was asked for comes back
     const user = await prisma.mutation.createUser({
@@ -23,16 +40,16 @@ const Mutation = {
 
     return {
       user,
-      token: jwt.sign({
-        userId: user.id
-      }, 'whatsecretis?')
-    }
-
+      token: jwt.sign(
+        {
+          userId: user.id
+        },
+        "whatsecretis?"
+      )
+    };
   },
 
-  async deleteUser(parent, args, {
-    prisma
-  }, info) {
+  async deleteUser(parent, args, { prisma }, info) {
     // const userExists = await prisma.exists.User({
     //   id: args.id
     // });
@@ -41,113 +58,112 @@ const Mutation = {
 
     // code above not necessary, left for reference sake
 
-    return await prisma.mutation.deleteUser({
-      where: {
-        id: args.id
-      }
-    }, info);
-  },
-
-  updateUser(parent, args, {
-    prisma
-  }, info) {
-    return prisma.mutation.updateUser({
-      where: {
-        id: args.id
-      },
-      data: args.data
-    }, info)
-  },
-
-  createPost(parent, args, {
-    prisma,
-  }, info) {
-
-    return prisma.mutation.createPost({
-      data: {
-        title: args.data.title,
-        body: args.data.body,
-        published: args.data.published,
-        author: {
-          connect: {
-            id: args.data.author
-          }
+    return await prisma.mutation.deleteUser(
+      {
+        where: {
+          id: args.id
         }
-      }
-    }, info)
-
-  },
-
-  async deletePost(parent, args, {
-    prisma
-  }, info) {
-    return await prisma.mutation.deletePost({
-      where: {
-        id: args.id
-      }
-    }, info)
-  },
-
-  updatePost(parent, {
-    id,
-    data
-  }, {
-    prisma
-  }, info) {
-    return prisma.mutation.updatePost({
-      where: {
-        id: id
       },
-      data: data
-    }, info)
+      info
+    );
   },
 
-  createComment(parent, args, {
-    prisma
-  }, info) {
-    return prisma.createComment({
-      data: {
-        text: args.data.text,
-        author: {
-          connect: {
-            id: args.data.author
-          }
+  updateUser(parent, args, { prisma }, info) {
+    return prisma.mutation.updateUser(
+      {
+        where: {
+          id: args.id
         },
-        post: {
-          connect: {
-            id: args.data.post
+        data: args.data
+      },
+      info
+    );
+  },
+
+  createPost(parent, args, { prisma }, info) {
+    return prisma.mutation.createPost(
+      {
+        data: {
+          title: args.data.title,
+          body: args.data.body,
+          published: args.data.published,
+          author: {
+            connect: {
+              id: args.data.author
+            }
           }
         }
-      }
-    }, info)
-  },
-
-  deleteComment(parent, args, {
-    prisma
-  }, info) {
-    return prisma.mutation.deleteComment({
-      where: {
-        id: args.id
-      }
-    }, info)
-  },
-
-  updateComment(parent, {
-    id,
-    data
-  }, {
-    prisma
-  }, info) {
-    return prisma.mutation.updateComment({
-      where: {
-        id: id
       },
-      data: data
-    }, info)
+      info
+    );
+  },
+
+  async deletePost(parent, args, { prisma }, info) {
+    return await prisma.mutation.deletePost(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+
+  updatePost(parent, { id, data }, { prisma }, info) {
+    return prisma.mutation.updatePost(
+      {
+        where: {
+          id: id
+        },
+        data: data
+      },
+      info
+    );
+  },
+
+  createComment(parent, args, { prisma }, info) {
+    return prisma.createComment(
+      {
+        data: {
+          text: args.data.text,
+          author: {
+            connect: {
+              id: args.data.author
+            }
+          },
+          post: {
+            connect: {
+              id: args.data.post
+            }
+          }
+        }
+      },
+      info
+    );
+  },
+
+  deleteComment(parent, args, { prisma }, info) {
+    return prisma.mutation.deleteComment(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+
+  updateComment(parent, { id, data }, { prisma }, info) {
+    return prisma.mutation.updateComment(
+      {
+        where: {
+          id: id
+        },
+        data: data
+      },
+      info
+    );
   }
 };
 
-export {
-  Mutation as
-  default
-};
+export { Mutation as default };
