871675c09df37427f5dc9b77050dc8a1248e0fed
sajicode <sajioloye@gmail.com>
2019-04-07 17:31:48 +0100

    lock down email field

--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,6 +1,20 @@
-const User = {
+import getUserId from '../utils/getUserId';
 
-}
+const User = {
+  email(parent, args, {
+    request
+  }, info) {
+    const userId = getUserId(request, false);
+
+    if (userId && userId === parent.id) {
+      return parent.email;
+    } else {
+      return null;
+    }
+  }
+};
+
+// parent is the User object
 
 export {
   User as
