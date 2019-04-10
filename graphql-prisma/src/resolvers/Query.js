380b35a7ff5265002214e659bb9b9fd14dabbb51
sajicode <sajioloye@gmail.com>
2019-04-10 23:12:45 +0100

    add cursor pagination for myPosts & comments

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -39,7 +39,10 @@ const Query = {
         author: {
           id: userId
         }
-      }
+      },
+      first: args.first,
+      skip: args.skip,
+      after: args.after
     }
 
     if (args.query) {
@@ -124,8 +127,12 @@ const Query = {
     db,
     prisma
   }, info) {
-
-    return prisma.query.comments(null, info);
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after
+    }
+    return prisma.query.comments(opArgs, info);
   }
 };
 
